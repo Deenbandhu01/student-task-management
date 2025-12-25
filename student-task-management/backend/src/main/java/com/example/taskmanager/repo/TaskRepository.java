98cd87fb0e6f3d7b7824ee54a68efb6e0fa782bb
Deen Bandhu <141038043+Deenbandhu01@users.noreply.github.com>
2025-12-25 18:19:23 +0530

    Delete student-task-management directory

--- student-task-management/backend/src/main/java/com/example/taskmanager/repo/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-
-package com.example.taskmanager.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.taskmanager.model.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {}
